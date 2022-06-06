cf9e483bf3d6e21b6d40ad5eec8d08fc84e4b020
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-06-06 21:33:17 +0800

    navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,42 @@
 <template>
-  <div class="p-6 flex bg-blue-50 justify-center">
-    <div class="">Navbar</div>
+
+  <div class="p-6 flex bg-blue-50 justify-between">
+    <div class="flex bg-blue-50">
+      <img src="src/assets/img/logo.png" class="object-cover h-20 w-100">
+    </div>
+    <div class="flex bg-blue-50 justify-centre flex-col " >
+      <div class="flex justify-center ">
+        <input class="" type="text" placeholder="Search..">
+      </div>
+      <div class="flex">
+        <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
+          <img src="src/assets/img/home.svg" class="w-10">
+          <a href="#home"  class="text-center text-[#2D3D8F]">Home</a>
+        </div>
+        <div class="text-black font-bold py-2 px-4 rounded; flex flex-col items-center ">
+          <img src="src/assets/img/university.svg" class="w-10">
+          <a href="#home"  class="text-center text-[#2D3D8F]">Universities</a>
+        </div>
+        <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
+          <img src="src/assets/img/moduleMapping.svg " class="w-10 text-[#2D3D8F]">
+          <a href="#home">Module Mapping</a>
+        </div>
+        <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
+          <img src="src/assets/img/community.svg " class="w-10 text-[#2D3D8F]">
+          <a href="#home">Community</a>
+        </div>
+      </div>
+    </div>
+    <div class="flex bg-blue-50 ">
+      <a href="#login" class="px-2 underline">Log In</a>
+      <a href="#signup" class="px-2 underline">Sign Up</a>
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
 
-<style></style>
+</script>
+
+<style scoped>
+</style>
