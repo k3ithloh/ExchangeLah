7011af1cb2377820510df486eba4ac00c4c74771
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-06-06 22:02:02 +0800

    Added router links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,27 +9,35 @@
         <input class="" type="text" placeholder="Search..">
       </div>
       <div class="flex">
-        <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
-          <img src="src/assets/img/home.svg" class="w-10">
-          <a href="#home"  class="text-center text-[#2D3D8F]">Home</a>
-        </div>
-        <div class="text-black font-bold py-2 px-4 rounded; flex flex-col items-center ">
-          <img src="src/assets/img/university.svg" class="w-10">
-          <a href="#home"  class="text-center text-[#2D3D8F]">Universities</a>
-        </div>
-        <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
-          <img src="src/assets/img/moduleMapping.svg " class="w-10 text-[#2D3D8F]">
-          <a href="#home">Module Mapping</a>
-        </div>
-        <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
-          <img src="src/assets/img/community.svg " class="w-10 text-[#2D3D8F]">
-          <a href="#home">Community</a>
-        </div>
+        <router-link to="/">
+          <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
+            <img src="src/assets/img/home.svg" class="w-10">
+            <a href="#home"  class="text-center text-[#2D3D8F]">Home</a>
+          </div>
+        </router-link>
+        <router-link to="#univeristy">
+          <div class="text-black font-bold py-2 px-4 rounded; flex flex-col items-center ">
+            <img src="src/assets/img/university.svg" class="w-10">
+            <a href="#home"  class="text-center text-[#2D3D8F]">Universities</a>
+          </div>
+        </router-link>
+        <router-link to="#moduleMapping">
+          <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
+            <img src="src/assets/img/moduleMapping.svg " class="w-10">
+            <a href="#home" class="text-center text-[#2D3D8F]">Module Mapping</a>
+          </div>
+        </router-link>
+        <router-link to="#community">
+          <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
+            <img src="src/assets/img/community.svg " class="w-10">
+            <a href="#home" class="text-center text-[#2D3D8F]">Community</a>
+          </div>
+        </router-link>
       </div>
     </div>
     <div class="flex bg-blue-50 ">
-      <a href="#login" class="px-2 underline">Log In</a>
-      <a href="#signup" class="px-2 underline">Sign Up</a>
+      <router-link to="/login" class="px-2 underline">Log in</router-link>
+      <router-link to="/signup" class="px-2 underline">Sign Up</router-link>
     </div>
   </div>
 </template>
