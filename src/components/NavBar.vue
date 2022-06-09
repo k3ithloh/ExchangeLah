46608fe14bab177b2a131f369652bd3084fd2cdc
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-06-09 15:55:42 +0800

    Search Bar, Changed icons and update function

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,36 +1,37 @@
-<template>
 
-  <div class="p-6 flex bg-blue-50 justify-between">
-    <div class="flex bg-blue-50">
+<template>
+  <div class="flex bg-blue-50 justify-between">
+    <div class="flex">
       <img src="src/assets/img/exchangeLah.png" class="object-cover h-20 w-100">
     </div>
-    <div class="flex bg-blue-50 justify-centre flex-col " >
-      <div class="flex justify-center ">
-        <input class="" type="text" placeholder="Search..">
+    <div class="flex bg-blue-50 justify-centre flex-col pt-5" >
+      <div class="flex justify-center h-12 rounded border-2 border-[#D3D6E7]">
+        <input @input="search" class="w-full bg-blue-50" type="search" placeholder="Search..">
+        <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">search</span>
       </div>
       <div class="flex">
         <router-link to="/">
-          <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
-            <img src="src/assets/img/home.svg" class="w-10">
-            <a href="#home"  class="text-center text-[#2D3D8F]">Home</a>
+          <div class="py-2 px-4 flex flex-col items-center ">
+            <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">home</span>
+            <a href="#home"  class="text-center text-[#2D3D8F] font-bold">Home</a>
           </div>
         </router-link>
         <router-link to="#univeristy">
-          <div class="text-black font-bold py-2 px-4 rounded; flex flex-col items-center ">
-            <img src="src/assets/img/university.svg" class="w-10">
-            <a href="#home"  class="text-center text-[#2D3D8F]">Universities</a>
+          <div class="py-2 px-4 flex flex-col items-center ">
+            <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">school</span>
+            <a href="#home"  class="text-center text-[#2D3D8F] font-bold">Universities</a>
           </div>
         </router-link>
         <router-link to="#moduleMapping">
-          <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
-            <img src="src/assets/img/moduleMapping.svg " class="w-10">
-            <a href="#home" class="text-center text-[#2D3D8F]">Module Mapping</a>
+          <div class="py-2 px-4 flex flex-col items-center ">
+          <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">map</span>
+            <a href="#home" class="text-center text-[#2D3D8F] font-bold">Module Mapping</a>
           </div>
         </router-link>
         <router-link to="#community">
-          <div class="text-black font-bold py-2 px-4 rounded flex flex-col items-center ">
-            <img src="src/assets/img/community.svg " class="w-10">
-            <a href="#home" class="text-center text-[#2D3D8F]">Community</a>
+          <div class="py-2 px-4 flex flex-col items-center ">
+            <span class="material-symbols-outlined text-4xl text-[#2D3D8F] ">forum</span>
+            <a href="#home" class="text-center text-[#2D3D8F] font-bold">Community</a>
           </div>
         </router-link>
       </div>
@@ -43,7 +44,15 @@
 </template>
 
 <script>
-
+export default{
+    props: ['modelValue'],
+    methods: {
+    search: function () {
+      alert('Searching')
+      // `event` is the native DOM event
+    }
+  }
+}
 </script>
 
 <style scoped>
