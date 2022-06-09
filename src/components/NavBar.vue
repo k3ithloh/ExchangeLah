fdd84917d60eefd623cbcf9a48c0b5b07ce853fa
Tyler Lian <65641550+anahopbob@users.noreply.github.com>
2022-06-09 20:33:11 +0800

    Changed icons to awesome font

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,44 +1,46 @@
 
 <template>
   <div class="flex bg-blue-50 justify-between">
-    <div class="flex">
+    <div class="flex justify-center items-center">
       <img src="src/assets/img/exchangeLah.png" class="object-cover h-20 w-100">
     </div>
     <div class="flex bg-blue-50 justify-centre flex-col pt-5" >
       <div class="flex justify-center h-12 rounded border-2 border-[#D3D6E7]">
         <input @input="search" class="w-full bg-blue-50" type="search" placeholder="Search..">
-        <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">search</span>
+        <div class="flex justify-center items-center">
+          <i class="fa-solid fa-magnifying-glass fa-2x text-[#2D3D8F]"></i>
+        </div>
       </div>
       <div class="flex">
         <router-link to="/">
-          <div class="py-2 px-4 flex flex-col items-center ">
-            <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">home</span>
+          <div class="py-4 px-4 flex flex-col items-center ">
+            <i class="fa-solid fa-house fa-2x text-[#2D3D8F]"></i>
             <a href="#home"  class="text-center text-[#2D3D8F] font-bold">Home</a>
           </div>
         </router-link>
         <router-link to="#univeristy">
-          <div class="py-2 px-4 flex flex-col items-center ">
-            <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">school</span>
+          <div class="py-4 px-4 flex flex-col items-center ">
+            <i class="fa-solid fa-user-graduate fa-2x text-[#2D3D8F]"></i>
             <a href="#home"  class="text-center text-[#2D3D8F] font-bold">Universities</a>
           </div>
         </router-link>
         <router-link to="#moduleMapping">
-          <div class="py-2 px-4 flex flex-col items-center ">
-          <span class="material-symbols-outlined text-4xl text-[#2D3D8F]">map</span>
+          <div class="py-4 px-4 flex flex-col items-center ">
+            <i class="fa-solid fa-diagram-project fa-2x text-[#2D3D8F]"></i>
             <a href="#home" class="text-center text-[#2D3D8F] font-bold">Module Mapping</a>
           </div>
         </router-link>
         <router-link to="#community">
-          <div class="py-2 px-4 flex flex-col items-center ">
-            <span class="material-symbols-outlined text-4xl text-[#2D3D8F] ">forum</span>
+          <div class="py-4 px-4 flex flex-col items-center ">
+            <i class="fa-solid fa-comments fa-2x text-[#2D3D8F]"></i>
             <a href="#home" class="text-center text-[#2D3D8F] font-bold">Community</a>
           </div>
         </router-link>
       </div>
     </div>
-    <div class="flex bg-blue-50">
-      <router-link to="/login" class="px-2 underline">Log in</router-link>
-      <router-link to="/signup" class="px-2 underline">Sign Up</router-link>
+    <div class="flex bg-blue-50 py-4">
+      <router-link to="/login" class="px-2 underline font-bold text-[#2D3D8F]">Log in</router-link>
+      <router-link to="/signup" class="px-2 underline font-bold text-[#2D3D8F]">Sign Up</router-link>
     </div>
   </div>
 </template>
