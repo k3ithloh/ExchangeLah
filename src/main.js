a23cd6a41a3c8b86bf44af71c96234274734300c
ngkangting <benji.ngkt@gmail.com>
2022-06-18 13:12:28 +0800

    Cleanup List of Universities Page

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { router } from "./router/index";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
+import "flowbite";
+import "vue-pagination-tw/styles";
 
 const app = createApp(App).use(router);
 app.mount("#app");
