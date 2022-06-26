e384263c6423af9c0a33b4a57d5188a6db047aea
Desiree Tan <desireetan@Desirees-MacBook-Pro.local>
2022-06-26 18:20:21 +0800

    Responsive Login and Signup Page

---
+++ src/views/UniversityInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="px-10 pt-3 text-2xl font-medium">University Name</h1>
+  </div>
+  <div class="container mx-auto">
+    <div
+      class="grid-cols-4 p-10 space-y-2 lg:space-y-0 lg:grid lg:gap-3 lg:grid-rows-3"
+    >
+      <div class="w-full rounded">
+        <img
+          src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=989&q=80"
+          alt="image"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "UniversityInfo",
+  components: {},
+};
+</script>
