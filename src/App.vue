9392144d47abad6e3f22c90f426ae7096aa64020
ngkangting <benji.ngkt@gmail.com>
2022-07-15 22:53:11 +0800

    Testing Endpoints

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div>
     <NavBar />
     <router-view class="px-8"></router-view>
+    {{ response }}
   </div>
 </template>
 
@@ -12,6 +13,28 @@ export default {
   name: "App",
   components: {
     NavBar,
+  },
+  data() {
+    return {
+      response: null,
+    };
+  },
+  mounted() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      await fetch("https://fakestoreapi.com/products", {
+        headers: { "Content-type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          this.response = response;
+        })
+        .catch((error) => {
+          console.log("Looks like there was a problem: \n", error);
+        });
+    },
   },
 };
 </script>
